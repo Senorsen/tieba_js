a5d70540a8d0d9134851dd1365bb8a543c647cbb
Sensor <zhs490770@foxmail.com>
2013-09-07 00:12:04 +0800

    auto fetch tbs

--- 吧内搜索删贴.js
+++ 吧内搜索删贴.js
@@ -1,8 +1,11 @@
 ﻿//到浙江大学吧内搜索页；其他吧也能用，但是tbs，贴吧id什么的都要自己改一下。连云小森森
+//    http://tieba.baidu.com/dc/common/tbs
+var tbs;
+$.getJSON("http://tieba.baidu.com/dc/common/tbs","",function(data){tbs=data['tbs'];});
 var o=new Array();
 var thisdel=0;
 var _scb=function(data){var txt='';if(data['no']!=0)txt=thisdel+' 失败：'+data['no'];else txt=thisdel+' 成功~';console.debug(txt);setTimeout(delnext,250);}
-var del2=function(tid,pid){$.post('/f/commit/post/delete',{commit_fr:"pb",ie:"utf-8",tbs:"be4912e5fe68b2581375225140",kw:"浙江大学",fid:26074,tid:tid,is_vipdel:0,pid:pid,is_finf:false},_scb,"json");};
+var del2=function(tid,pid){$.post('/f/commit/post/delete',{commit_fr:"pb",ie:"utf-8",tbs:tbs,kw:"浙江大学",fid:26074,tid:tid,is_vipdel:0,pid:pid,is_finf:false},_scb,"json");};
 var del=function(lnk){var tid='',pid='';var rg=lnk.match(/\/p\/(\d+)[?]pid=(\d+)/);
 tid=rg[1];pid=rg[2];del2(tid,pid);}
 var addlist=function(){
